a201d87e3713cfe13d66931fa08d8f74e4f1edce
jmihalsky <jmihalsky@gmail.com>
2018-11-16 22:07:29 -0800

    work save

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [
+    {
+        name: "Don",
+        photo: "https://cdn.cnn.com/cnnnext/dam/assets/181107105709-01-trump-play-button-exlarge-169.jpg",
+        scores: [1,2,1,5,1,5,1,1,1,3]
+    },
+    {
+        name: "Joe",
+        photo: "https://moneyinc.com/wp-content/uploads/2018/05/Joe-Rogan-750x393.jpg",
+        scores: [5,2,2,5,3,1,2,4,5,5]
+    },
+    {
+        name: "Judy",
+        photo: "https://www.biography.com/.image/t_share/MTMzODY1ODEyODc1MzE4NTQ2/judge_judyjpg.jpg",
+        scores: [3,5,4,3,3,2,5,2,1,3]
+    },
+    {
+        name: "Bob",
+        photo: "https://superawesomevectors.com/wp-content/uploads/2017/05/bob-belcher-bobs-burgers-vector-800x566.jpg",
+        scores: [5,1,2,3,2,3,3,2,3,2]
+    }
+];
+
+module.exports = friends;
