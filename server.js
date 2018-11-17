6463f06144cdfcae88cf253bfadf9d9f3564dd55
jmihalsky <jmihalsky@gmail.com>
2018-11-16 18:24:06 -0800

    html and express server work done

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require("./app/routing/htmlRoutes")(app);
+// require("./app/routing/apiRoutes")(app);
+
+// app.get("/",function(req,res) {
+//     res.sendfile(path.join(__dirname, "./app/public/home.html"));
+// });
+
+app.listen(PORT, function(){
+    console.log("App listening on http://localhost:" + PORT)
+});
+
